722a3747dcb5bfaeae8221122d7381a1be6c5ef9
coolshubh4 <shubham.tripathi4@outlook.com>
2015-09-06 23:05:38 +0530

    Add MapView onto Storyboard

--- VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TravelLocationsMapViewController.swift
 //  VirtualTourist
 //
 //  Created by Shubham Tripathi on 06/09/15.
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
