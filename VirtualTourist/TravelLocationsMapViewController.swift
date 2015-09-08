a7c103b9955b4d7c144f1b85f87ef7230c882a43
coolshubh4 <shubham.tripathi4@outlook.com>
2015-09-08 22:23:17 +0530

    Add PinAnnotation

--- VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -12,18 +12,34 @@ import MapKit
 class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    
+    var longPress: UILongPressGestureRecognizer!
+    var mapAnnotations = [MKPointAnnotation]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.hidden = true
+        longPress = UILongPressGestureRecognizer(target: self, action: "dropPin:")
+        longPress.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(longPress)
         
         mapView.delegate = self
     }
     
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dropPin(gesture: UILongPressGestureRecognizer) {
+        
+        var mapAnnotation = MKPointAnnotation()
+        let touchPoint: CGPoint = gesture.locationInView(mapView)
+        let touchPointCoordinates: CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        
+        if UIGestureRecognizerState.Began == gesture.state {
+            println("Pin dropped: \(touchPointCoordinates.latitude) -- \(touchPointCoordinates.longitude)")
+            mapAnnotation.coordinate = touchPointCoordinates
+            mapAnnotations.append(mapAnnotation)
+        }
+        dispatch_async(dispatch_get_main_queue()) {
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            self.mapView.addAnnotations(self.mapAnnotations)
+        }
     }
 
 }
