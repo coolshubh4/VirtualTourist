45d7bddd9dd489427bf360540958bfa3af810969
coolshubh4 <shubham.tripathi4@outlook.com>
2015-09-12 10:54:13 +0530

    Implemented PinDrop

--- VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/TravelLocationsMapViewController.swift
@@ -13,7 +13,9 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     var longPress: UILongPressGestureRecognizer!
-    var mapAnnotations = [MKPointAnnotation]()
+    //var mapAnnotations = [MKPointAnnotation]()
+    var pinDropLocation: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,19 +29,54 @@ class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
     
     func dropPin(gesture: UILongPressGestureRecognizer) {
         
-        var mapAnnotation = MKPointAnnotation()
         let touchPoint: CGPoint = gesture.locationInView(mapView)
         let touchPointCoordinates: CLLocationCoordinate2D = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        //getPinDropLocation(CLLocation(latitude: touchPointCoordinates.latitude, longitude: touchPointCoordinates.longitude))
         
         if UIGestureRecognizerState.Began == gesture.state {
-            println("Pin dropped: \(touchPointCoordinates.latitude) -- \(touchPointCoordinates.longitude)")
-            mapAnnotation.coordinate = touchPointCoordinates
-            mapAnnotations.append(mapAnnotation)
+            CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: touchPointCoordinates.latitude, longitude: touchPointCoordinates.longitude)) { placemarks, error in
+                if error != nil {
+                    let errMsg: String? = (error!.code == 8) ? "User location cannot be located on the map. Please retry with a different location" : "No network connection available"
+                    println("\(errMsg)")
+                } else {
+                    if placemarks == nil {
+                        println("No Lacation Data")
+                        self.pinDropLocation = nil
+                    } else {
+                        var placemark = placemarks[0] as! CLPlacemark
+                        println("placemark addressDictionary - \(placemark.addressDictionary)")
+                        let name = placemark.addressDictionary["Name"] as! String
+                        let area = placemark.addressDictionary["SubAdministrativeArea"] as! String
+                        self.pinDropLocation = name + ", " + area
+                        let mapAnnotation = MKPointAnnotation()
+                        mapAnnotation.coordinate = touchPointCoordinates
+                        mapAnnotation.title = "Dropped Pin"
+                        mapAnnotation.subtitle = self.pinDropLocation
+                        self.mapView.addAnnotation(mapAnnotation)
+                    }
+                }
+            }
         }
-        dispatch_async(dispatch_get_main_queue()) {
-            self.mapView.removeAnnotations(self.mapView.annotations)
-            self.mapView.addAnnotations(self.mapAnnotations)
+    }
+    
+    // MARK: - MKMapViewDelegate
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        let reuseId = "mapAnnotation"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+            pinView!.animatesDrop = true
+            pinView!.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIButton
         }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
     }
 
 }
