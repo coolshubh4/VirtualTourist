26f4aa7f563394a271a8c7107a80341d53dc0f92
coolshubh4 <shubham.tripathi4@outlook.com>
2015-09-14 23:07:01 +0530

    Done - VirtualTourist client

--- VirtualTourist/PhotoAlbumViewController.swift
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -13,6 +13,19 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate{
     
     
     @IBOutlet weak var pinDroppedMapRegion: MKMapView!
-    
     var pin: Pin!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pinDroppedMapRegion.delegate = self
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let mapRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(pin.pinLatitude as! Double, pin.pinLongitude as! Double), MKCoordinateSpanMake((pin.pinLatitude as! Double)/2, (pin.pinLongitude as! Double)/2))
+        pinDroppedMapRegion.setRegion(mapRegion, animated: true)
+    }
+    
+    
 }
--- VirtualTourist/VirtualTouristClient.swift
+++ VirtualTourist/VirtualTouristClient.swift
@@ -7,3 +7,112 @@
 //
 
 import Foundation
+
+class VirtualTouristClient: NSObject {
+    
+    var session: NSURLSession
+    
+    override init() {
+        session = NSURLSession.sharedSession()
+        super.init()
+    }
+    
+    struct constants {
+        static let baseURL = "https://api.flickr.com/services/rest/"
+        static let methodName = "flickr.photos.search"
+        static let api_key = "1a7285e8e24c702c34edd13ad198c382"
+        static let extras = "url_q"
+        static let media = "photos"
+        static let format = "json"
+        static let nojsoncallback = "1"
+    }
+    
+    func getPhotosFromFlickr(latitude: Double, longitude: Double, completionHandler: (success: Bool, photoURLArray: [String]?,errorString: String?) -> Void) {
+        
+        let parameters = [
+            "method" : constants.methodName,
+            "api_key" : constants.api_key,
+            "extras" : constants.extras,
+            "media" : constants.media,
+            "bbox" : createBoundingBoxString(latitude, longitude: longitude),
+            "format" : constants.format,
+            "nojsoncallback" : constants.nojsoncallback]
+        
+        let url = NSURL(string: (constants.baseURL + escapedParameters(parameters)))
+        let request = NSURLRequest(URL: url!)
+        
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                completionHandler(success: false, photoURLArray: nil, errorString: "Not able to request data - \(error)")
+            } else {
+                VirtualTouristClient.parseJSONWithCompletionHandler(data) { result, error in
+                    if error != nil {
+                        completionHandler(success: false, photoURLArray: nil, errorString: "Could not parse data - \(error)")
+                    } else {
+                        if let photos = result["photos"] as? NSDictionary {
+                            if photos["total"] as! String == "0" {
+                                completionHandler(success: true, photoURLArray: nil, errorString: nil)
+                            } else if let photoData = photos["photo"] as? [[String : AnyObject]]{
+                                var photoURLArray = [String]()
+                                
+                                for photo in photoData {
+                                    if let url = photo["url_q"] as? String {
+                                        photoURLArray.append(url)
+                                    }
+                                }
+                                completionHandler(success: true, photoURLArray: photoURLArray, errorString: nil)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    // Create the BBOX necessary for the flickr API call
+    func createBoundingBoxString(latitude: Double, longitude: Double) -> String {
+        
+        
+        let bottom_left_lon = (longitude - 0.5)
+        let bottom_left_lat = (latitude - 0.5)
+        let top_right_lon = (longitude + 0.5)
+        let top_right_lat = (latitude + 0.5)
+        
+        return "\(bottom_left_lon),\(bottom_left_lat),\(top_right_lon),\(top_right_lat)"
+    }
+    
+    // Escape function parameters
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+        }
+        return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
+    }
+    
+    // JSON parsing
+    class func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        var parsingError: NSError? = nil
+        let parsedResult: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError)
+        if let error = parsingError {
+            completionHandler(result: nil, error: error)
+        } else {
+            completionHandler(result: parsedResult, error: nil)
+        }
+    }
+    
+    class func sharedInstance() -> VirtualTouristClient {
+        struct Singleton {
+            static var instance = VirtualTouristClient()
+        }
+        return Singleton.instance
+    }
+}
